71cbdb38499c7d0c8cffe73140a15047de618613
박성현 <qkr970504@gmail.com>
2025-08-05 18:22:07 +0900

    Component 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
 import './App.css';
 
+function Header(props) {
+    return (
+        <header>
+            <h1><a href="/" onClick={(event) => {
+                event.preventDefault();
+                props.onChangeMode();
+            }}>{props.title}</a></h1>
+        </header>
+    );
+}
+
+function Nav(props) {
+    const list = []
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i]
+        list.push(<li key={t.id}>
+            <a id={t.id} href={'/read/' + t.id} onClick={event=>{
+                event.preventDefault();
+                props.onChangeMode(event.target.id);
+            }}>{t.title}</a>
+        </li>)
+    }
+
+    return (
+        <nav>
+            <ol>
+                {list}
+            </ol>
+        </nav>
+    );
+}
+
+function Article(props) {
+    return (
+        <article>
+            <h2>Welcome</h2>
+            Hello, WEB
+        </article>
+    );
+}
+
 function App() {
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'javascript', body:'javascript is ...'}
+    ];
+
     return (
         <div className="App">
-            Hello, World!
+            <Header title="REACT" onChangeMode={() => {
+                alert('Header');
+            }}/>
+            <Nav topics={topics} onChangeMode={(id) => {
+                alert(id);
+            }}/>
+            <Article/>
         </div>
     );
 }
