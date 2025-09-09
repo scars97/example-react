9a7223748e0a0a5d200325db259502c09e9f2001
박성현 <qkr970504@gmail.com>
2025-09-09 16:46:06 +0900

    create 기능 추가
    - useState 가 배열인 경우, 복사 후 사용

--- src/App.js
+++ src/App.js
@@ -42,15 +42,31 @@ function Article(props) {
     );
 }
 
+function Create(props) {
+    return <article>
+        <h2>Create</h2>
+        <form onSubmit={event => {
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onCreate(title, body);
+        }}>
+            <p><input type="text" name="title" placeholder="title"/></p>
+            <p><textarea name="body" placeholder="body"/></p>
+            <p><input type="submit" value="Create"/></p>
+        </form>
+    </article>
+}
+
 function App() {
     const [mode, setMode] = useState('WELCOME');
     const [id, setId] = useState(null);
-
-    const topics = [
+    const [nextId, setNextId] = useState(4);
+    const [topics, setTopics] = useState([
         {id:1, title:'html', body:'html is ...'},
         {id:2, title:'css', body:'css is ...'},
         {id:3, title:'javascript', body:'javascript is ...'}
-    ];
+    ]);
 
     let content = null;
     if (mode === 'WELCOME') {
@@ -64,6 +80,17 @@ function App() {
             }
         }
         content = <Article title={title} body={body}/>
+    } else if (mode === 'CREATE') {
+        content = <Create onCreate={(title, body) => {
+            const newTopic = {id:nextId, title:title, body:body};
+            const newTopics = [...topics]
+            newTopics.push(newTopic);
+
+            setTopics(newTopics);
+            setMode('READ');
+            setId(nextId);
+            setNextId(nextId + 1);
+        }}/>
     }
 
     return (
@@ -76,6 +103,10 @@ function App() {
                 setId(id)
             }}/>
             {content}
+            <a href="/create" onClick={event => {
+                event.preventDefault();
+                setMode('CREATE');
+            }}>Create</a>
         </div>
     );
 }
